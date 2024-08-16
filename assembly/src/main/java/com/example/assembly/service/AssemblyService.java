d3e39a9116bedf752a29c7479b16d7ffc1bc39d3
ramanadas <104691675+ramanadas@users.noreply.github.com>
2024-08-16 18:30:45 +0530

    Add files via upload

--- assembly/src/main/java/com/example/assembly/service/AssemblyService.java
+++ assembly/src/main/java/com/example/assembly/service/AssemblyService.java
@@ -2,16 +2,21 @@ package com.example.assembly.service;
 
 import com.example.assembly.entity.Program;
 import com.example.assembly.repository.ProgramRepository;
+import com.example.assembly.repository.RegistarRepo;
 import com.example.assembly.util.PraseCommand;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 @Service
 public class AssemblyService {
 
+    @Autowired
+    private RegistarRepo registarRepo;
     @Autowired
     private PraseCommand praseCommand;
     @Autowired
@@ -22,9 +27,40 @@ public class AssemblyService {
     @Transactional
     public String execute(String input) {
         String res = "";
+        boolean ifStarted=false;
+        boolean first = true;
+        String[] temp={};
+        List<String> list = new ArrayList<>();
         try {
             List<String> commands = praseCommand.parse(input);
             for(String command : commands){
+                if(command.startsWith("IFGT AND IF")){
+                   ifStarted=true;
+                   continue;
+                }
+                if(command.startsWith("ENDIF")){
+                    ifStarted=false;
+                    continue;
+                }
+                if(ifStarted){
+                    if(first){
+                        temp= command.split(" ");
+                    }
+
+                    if(registarRepo.getById(temp[1]).getValue()>Integer.parseInt(temp[2])){
+                        if(first){
+                            first=false;
+                        }else {
+                            list.add(command);
+                        }
+                    }
+                    first=false;
+                    continue;
+                }
+                list.add(command);
+
+            }
+            for(String command: list){
                 String s = executeService.executeCommand(command);
                 res=res.concat(s +"\n");
             }
